ff8a84fc337f988bda78d984b9da42554a8abf6e
LioTree <uc0110@163.com>
2023-02-13 10:29:16 +0800

    delete .git stupidly...finish lab3-lab6 before

---
+++ lab03/factorial.s
@@ -0,0 +1,40 @@
+.globl factorial
+
+.data
+n: .word 8
+
+.text
+main:
+    la t0, n
+    lw a0, 0(t0)
+    jal ra, factorial
+
+    addi a1, a0, 0
+    addi a0, x0, 1
+    ecall # Print Result
+
+    addi a1, x0, '\n'
+    addi a0, x0, 11
+    ecall # Print newline
+
+    addi a0, x0, 10
+    ecall # Exit
+
+factorial:
+    # YOUR CODE HERE
+    addi sp,sp,-8
+    sw s0,0(sp)
+    sw s1,4(sp)
+    add s0,x0,a0
+    addi s1,x0,1
+loop:
+    beq s0,x0, exit
+    mul s1,s1,s0
+    addi s0,s0,-1
+    jal x0,loop
+exit:
+    add a0,x0,s1
+    lw s0,0(sp)
+    lw s1,4(sp)
+    addi sp,sp,8
+    jr ra
